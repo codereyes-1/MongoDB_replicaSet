276bd561a4ead6947af933a87d6550435543b41f
Julio Daniel <80291124+codereyes-1@users.noreply.github.com>
2021-05-24 12:25:05 -0700

    Update replSet_commands.sh

--- replSet_commands.sh
+++ replSet_commands.sh
@@ -5,7 +5,10 @@ mongod --replSet m103rs --logpath "rps1.log" --dbpath rps1 --port 27017 &
 mongod --replSet m103rs --logpath "rps2.log" --dbpath rps2 --port 27018 & 
 mongod --replSet m103rs --logpath "rps3.log" --dbpath rps3 --port 27019 &
 
-# replSet member configuration
+# connect to Mongo
+mongo
+
+# var for replSet member configuration
 config = { _id: "m103rs", members:[
 			{_id: 0, host: "localhost:27017"},
 			{_id: 1, host: "localhost:27018"},
@@ -17,5 +20,7 @@ rs.initiate(config);
 # verify replSet status of each member
 rs.status();
 
-# connect to the primary
+
+
+# connect to cluster after primary node is down using this command. Will find a former secondary as the new primary after elections.
 mongo --host m103rs/localhost:27017,localhost:27018,localhost:27019
